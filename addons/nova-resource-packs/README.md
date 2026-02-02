fcbb40afcd24d6875c1e7d8ad99685f4c8518b27
NovaProot <ksawex17@gmail.com>
2026-02-02 18:33:35 +0100

    Added NovaScale and descriptions to NovaAnimSprite2D, also changed my username

---
+++ addons/nova-resource-packs/README.md
@@ -0,0 +1,1 @@
+When making a script for a NovaNode use _nova_ready() instead of _ready() beacuse it will break the node's functionality
--- addons/nova-resource-packs/nodes/animations/NovaAnimatedSprite2D.gd
+++ addons/nova-resource-packs/nodes/animations/NovaAnimatedSprite2D.gd
@@ -1,29 +1,32 @@
 extends AnimatedSprite2D
 
+## AnimationId from your base data.json [code]"animations"[/code] part[br]
+## its the [code]AnimationId[/code].AnimationName part of the id, if its without [code].[/code] then its just the id
 @export var AnimationId: StringName = &"missing"
+## AnimationName from your base data.json [code]"animations"[/code] part[br]
+## its the AnimationId.[code]AnimationName[/code] part of the id, it its without [code].[/code] then its [code]&"default"[/code]
 @export var AnimationName: StringName = &"default"
-@export var DefaultAnimationName: StringName = &"default"
+## if the animation will play when its ready in the scene
 @export var Autoplay: bool = false
+## if it should duplicate the animation from NovaAnimation.Animations instead of reusing[br]
+## it's needed to edit the sprite_frames without breaking other nodes that use this animation
 @export var DuplicateSpriteFrames: bool = false
-@export var BaseTextureSizeId: StringName = &"missing"
-@export var IsBaseTextureSheet: bool = false
-var BaseTextureSize: Vector2 = Vector2(64, 64)
+## base size of a single frame in your animation, all frames will be scaled to this size
+@export var BaseFrameSize: Vector2 = Vector2(64, 64)
+## use this instead of scale when editing the scale in-game, beacuse if you use [code]scale[/code] it will get overwritten
+@export var NovaScale: Vector2 = Vector2(0.0, 0.0)
 
+## use _nova_ready() unless you want to edit how the animation is loaded
 func _ready() -> void:
-	BaseTextureSize = NovaTexture.TextureSizes[BaseTextureSizeId]
-	
 	sprite_frames = NovaAnimation.get_animation(AnimationId, DuplicateSpriteFrames)
-	
-	if IsBaseTextureSheet:
-		var frames: Vector2 = Vector2(NovaAnimation.BaseAnimationsData[AnimationId].get("frame-x", 2), NovaAnimation.AnimationsData[AnimationId].get("frame-y", 2))
-		BaseTextureSize /= frames
-	_update_scale()
 	
 	NovaAnimation.ReloadAnimation.connect(_reload_animation)
 	frame_changed.connect(_update_scale)
+	animation_changed.connect(_update_scale)
 	
 	if Autoplay:
 		play(AnimationName)
+	_update_scale()
 	await _nova_ready()
 
 
@@ -33,7 +36,7 @@ func _nova_ready() -> void:
 
 func _update_scale() -> void:
 	var texture: Texture = sprite_frames.get_frame_texture(animation, frame)
-	scale = BaseTextureSize / texture.get_size()
+	scale = BaseFrameSize / texture.get_size() * NovaScale
 
 
 func _reload_animation() -> void:
